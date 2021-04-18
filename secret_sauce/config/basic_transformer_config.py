0512bee8ccb21472ea15a8a50648e4e9fd2cf1c6
Evan Pierce <mrthinger@gmail.com>
2021-04-18 05:38:01 +0000

    implement basic transformer in notebook

--- secret_sauce/config/basic_transformer_config.py
+++ secret_sauce/config/basic_transformer_config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 class BasicTransformerConfig():
     ff_dim: int = 2048
 
-    heads_num: int = 6
+    heads_num: int = 4
     blocks_num: int = 4
 
     dropout: int = 0.0
--- secret_sauce/network/basic_transformer/transformer.py
+++ secret_sauce/network/basic_transformer/transformer.py
@@ -9,40 +9,57 @@ from torchtyping import TensorType
 
 # https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 class PositionalEncoding(nn.Module):
-
-    def __init__(self, d_model: int, max_len:int =5000):
+    def __init__(self, d_model: int, max_len: int = 5000):
         super(PositionalEncoding, self).__init__()
 
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe)
+        self.register_buffer("pe", pe)
 
-    def forward(self, x: TensorType['batch', 'timestep', 'feature'], position_inds: TensorType['batch', 'timestep']):
-        x = x + self.pe[position_inds, :]
+    def forward(
+        self,
+        x: TensorType["batch", "timestep", "feature"],
+        position_inds: TensorType["batch", "timestep"],
+    ):
+
+        positional_encodings = self.pe[position_inds, :]
+
+        x = x + positional_encodings
         return x
 
 
-
 class BasicTransformer(nn.Module):
-
     def __init__(self, cfg: Config):
         super(BasicTransformer, self).__init__()
-        self.cfg = cfg.transformer
-        self.model_type = 'Transformer'
+        self.cfg = cfg
+        self.model_type = "Transformer"
         self.pos_encoder = PositionalEncoding(cfg.vqvae.embedding_dim, max_len=600000)
-        encoder_layers = TransformerEncoderLayer(cfg.vqvae.embedding_dim, cfg.transformer.heads_num, cfg.transformer.ff_dim, cfg.transformer.dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, cfg.transformer.blocks_num)
+        encoder_layers = TransformerEncoderLayer(
+            cfg.vqvae.embedding_dim,
+            cfg.transformer.heads_num,
+            cfg.transformer.ff_dim,
+            cfg.transformer.dropout,
+        )
+        self.transformer_encoder = TransformerEncoder(
+            encoder_layers, cfg.transformer.blocks_num
+        )
         self.codebook = nn.Embedding(cfg.vqvae.num_embeddings, cfg.vqvae.embedding_dim)
         self.predictions = nn.Linear(cfg.vqvae.embedding_dim, cfg.vqvae.num_embeddings)
 
         self.init_weights()
 
-    def generate_square_subsequent_mask(self, sz):
+    def generate_square_subsequent_mask(self, sz: int):
         mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        mask = (
+            mask.float()
+            .masked_fill(mask == 0, float("-inf"))
+            .masked_fill(mask == 1, float(0.0))
+        )
         return mask
 
     def init_weights(self):
@@ -53,10 +70,22 @@ class BasicTransformer(nn.Module):
     def load_embeddings(self, embeddings: torch.Tensor):
         self.codebook.weight.data = embeddings
 
+    def forward(
+        self,
+        src: TensorType["batch", "timestep"],
+        src_positions: TensorType["batch", "timestep"],
+        src_mask: TensorType["timestep", "timestep"],
+    ):
+        src: torch.Tensor = src
+        src = self.codebook(src) * math.sqrt(self.cfg.vqvae.embedding_dim)
+        
+        
+        src = self.pos_encoder(src, src_positions)
+        
+        # dont have gradients flow into codebook
+        src = src.detach()
 
-    def forward(self, src, src_mask):
-        src = self.codebook(src) * math.sqrt(self.codes_dim)
-        src = self.pos_encoder(src)
+        src = src.permute(1,0,2)
         output = self.transformer_encoder(src, src_mask)
         output = self.predictions(output)
         return output
