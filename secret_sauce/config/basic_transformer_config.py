b75b57c34067231fbe52a49937cb41f6e708b378
Evan Pierce <mrthinger@gmail.com>
2021-04-26 23:22:27 +0000

    tune params

--- secret_sauce/config/basic_transformer_config.py
+++ secret_sauce/config/basic_transformer_config.py
@@ -10,7 +10,7 @@ class BasicTransformerConfig():
     dropout: float = 0.1
 
 
-    window_size: int = 8192 * 4
+    window_size: int = 8192 * 2
     shift: int = 1 # this should be 1 when using autoregressive wrapper
 
     data_path: str = '/root/secret_sauce/savant-32000-compressed.pt'
