34fefef5a091769043df9300687ccaccb4201b76
Evan Pierce <mrthinger@gmail.com>
2021-04-27 06:29:12 +0000

    cache build pytorch fast transformers

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ poetry install
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
-pip install deepspeed==0.3.15 pytorch-fast-transformers==0.4.0
+pip install deepspeed==0.3.15 https://storage.googleapis.com/tensorbeat-public/pytorch_fast_transformers-0.4.0-cp39-cp39-linux_x86_64.whl
 ./setup_apex.sh
