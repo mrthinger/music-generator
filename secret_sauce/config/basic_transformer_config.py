48a0263b50906193a7699a5fc2f242f5d0244780
Evan Pierce <mrthinger@gmail.com>
2021-04-27 01:42:20 -0400

    small transformer

--- secret_sauce/config/basic_transformer_config.py
+++ secret_sauce/config/basic_transformer_config.py
@@ -2,15 +2,15 @@ from dataclasses import dataclass
 
 @dataclass
 class BasicTransformerConfig():
-    width: int = 1920
+    width: int = 800
 
-    heads_num: int = 1
-    blocks_num: int = 48
+    heads_num: int = 4
+    blocks_num: int = 18
 
     dropout: float = 0.1
 
 
-    window_size: int = 8192 * 2
+    window_size: int = 8192 // 2
     shift: int = 1 # this should be 1 when using autoregressive wrapper
 
     data_path: str = '/root/secret_sauce/savant-32000-compressed.pt'
