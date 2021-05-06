6c881f2746deb1ab2e7245f6f711ad52fcf832fa
Evan Pierce <mrthinger@gmail.com>
2021-05-06 22:26:53 +0000

    update deepspeed

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@ poetry install
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
-pip install deepspeed==0.3.15 https://storage.googleapis.com/tensorbeat-public/pytorch_fast_transformers-0.4.0-cp39-cp39-linux_x86_64.whl
-./setup_apex.sh
+pip install deepspeed==0.3.16 https://storage.googleapis.com/tensorbeat-public/pytorch_fast_transformers-0.4.0-cp39-cp39-linux_x86_64.whl
